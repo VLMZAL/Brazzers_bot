8c420c2ed620645e0b9761af04921f219f03a678
VLMZAL <leonardo.delmatto11@gmail.com>
2026-06-14 18:21:06 +0200

    Update war.js

--- commands/war.js
+++ commands/war.js
@@ -2,15 +2,28 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("war-tips")
-    .setDescription("Send war tips")
+    .setName("tips")
+    .setDescription("All tips")
     .addStringOption(option =>
-      option.setName("text")
-        .setDescription("The tip")
+      option.setName("type")
+        .setDescription("Select the type of tip you want")
         .setRequired(true)
-    ),
-  async execute(interaction) {
-    const text = interaction.options.getString("text");
-    await interaction.reply("⚔️ War tip: " + text);
+        .addChoices(
+          { name: "War", value: "war" },
+          { name: "General", value: "general" }
+        )
+    
+      ),
+  
+    async execute(interaction) {
+    const type = interaction.options.getString("type");
+    let text;
+
+    if (type === "war") {
+      text = "Use the right troops for the right base. For example, if you are attacking a base with a lot of air defenses, use more air troops.";
+    } else if (type === "general") {
+      text = "Here are some general tips for the game.";
+
+    await interaction.reply("⚔️ " + type + " tip: " + text);
   }
-};
+}};
