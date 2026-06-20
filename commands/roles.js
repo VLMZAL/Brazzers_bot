1f6ddd54bda587860dcb5b16bcc39060cc4f9170
VLMZAL <leonardo.delmatto11@gmail.com>
2026-06-20 14:32:21 +0200

    Create roles.js

---
+++ commands/roles.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, User } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("role")
+        .setDescription("Assign a role to a user")
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("add")
+                .setDescription("Add a role to a user")
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("remove")
+                .setDescription("Remove a role from a user")
+        )
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("Select the user")
+                .setRequired(true)
+                    
+        )
+        .addRoleOption(option =>
+            option
+                .setName("role")
+                .setDescription("Select the role to assign")
+                .setRequired(true)
+        ),
+
+        
+
+    async execute(interaction) {
+        const member = interaction.options.getMember("user");
+        const role = interaction.options.getRole("role");
+        const subcommand = interaction.options.getSubcommand();
+
+        if (subcommand === "add") {
+            await member.roles.add(role);
+            let reply = "";
+            reply += `<@${member.id}> has been assigned the role <@&${role.id}>! `;
+        } else if (subcommand === "remove") {
+            await member.roles.remove(role);
+            let reply = "";
+            reply += `the role <@${role.id}> has been removed from the user <@&${member.id}>! `;
+
+        } else {
+            let reply = "";
+            reply += `Invalid argument please use either "add" or "remove"`;
+        }
+
+        await interaction.reply(reply);
+    }
+};
